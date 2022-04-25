5703bdec05e5f58e3f4bfc1945b4de4e8f3c3061
gustavorizzon <gustavop.rizzon@gmail.com>
2022-04-25 14:56:48 -0300

    feat: reports

--- handler.js
+++ handler.js
@@ -1,15 +1,45 @@
-'use strict';
+const AWS = require('aws-sdk')
+const apiGateway = new AWS.APIGateway()
+const moment = require('moment')
 
-module.exports.hello = async (event) => {
+const hello = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: 'Hello world'
   };
-};
+}
+
+const usage = async (event) => {
+  const {
+    from, to, usagePlanId, keyId
+  } = event.queryStringParameters
+
+  const result = await apiGateway.getUsage({
+    startDate: moment(from).format('YYYY-MM-DD'),
+    endDate: moment(to).format('YYYY-MM-DD'),
+    usagePlanId,
+    keyId
+  }).promise()
+
+  console.log('Usage', result)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result, null, 2)
+  };
+}
+
+const usagePlans = async (event) => {
+  const result = await apiGateway.getUsagePlans().promise()
+  console.log('Usage Plans', result)
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result, null, 2)
+  };
+}
+
+module.exports = {
+  hello,
+  usage,
+  usagePlans
+}
--- requests/request.sh
+++ requests/request.sh
@@ -2,3 +2,14 @@ HOST=http://localhost:3000
 API_KEY="d41d8cd98f00b204e9800998ecf8427e"
 
 curl --silent -H "x-api-key: $API_KEY" $HOST/dev/hello
+
+curl --silent $HOST/dev/getUsagePlans
+
+# from getUsagePlans
+USAGE_PLAN_ID="0sqv4e"
+KEY_ID="ikthfgeduc"
+API_KEY="HoER0Do6rw83pE4DgqHmT2cgN7WIzQk2mueMVhOa"
+FROM="2022-04-25"
+TO="2022-04-26"
+
+curl --silent "$HOST/dev/getUsage?keyId=$KEY_ID&usagePlanId=$USAGE_PLAN_ID&from=$FROM&to=$TO"
